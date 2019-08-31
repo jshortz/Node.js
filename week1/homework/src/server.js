dd3397187357b16f0b23ce4f0720a763c61afe82
jshortz <jessica.shortz@gmail.com>
2019-08-31 05:00:11 +0200

    Homework complete; passes all tests, eslint for consistency

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,20 +1,37 @@
-'use strict';
+'use strict'
 
-const http = require('http');
+const http = require('http')
 
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
-function createServer(port) {
-  let state = 10;
+function createServer () {
+  let state = 10
 
-  const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
-  });
+  const server = http.createServer((req, res) => {
+    if (req.url == '/state') {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.write(JSON.stringify({ state: state }))
+    } else if (req.url == '/add') {
+      state += 1
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify({ state: state }))
+    } else if (req.url == '/subtract') {
+      state -= 1
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify({ state: state }))
+    } else if (req.url == '/reset') {
+      state = 10
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify({ state: state }))
+    } else {
+      res.writeHead(404, {'Content-Type': 'application/json'})
+      res.write(JSON.stringify({ error: 'Not found' }))
+    }
+    res.end()
+  })
 
-  return server;
+  return server
 }
 
-module.exports = {
-  createServer
-};
+module.exports = { createServer }
