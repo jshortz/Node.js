090014a33ca56071d57c5820891d4569895668c6
jshortz <jessica.shortz@gmail.com>
2019-09-04 14:53:41 +0200

    Fixed list for empty list, got reset function working

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 
 let toDoList = (fs.readFileSync('list.txt', 'utf8')).split('\n');
-let choice = process.argv[2];
+let choice = process.argv[2]; 
 let details = process.argv[3];
 
 // help or default (no parameter) function : outputs help section that lists all commands and what they do
@@ -20,19 +20,32 @@ function displayHelp() {
 // list: shows current to-dos or logical text if none have been created
 
 function displayList() {
-  console.log(toDoList)
+  const stats = fs.statSync("list.txt");
+  const fileSizeInBytes = stats.size;
+  if (fileSizeInBytes === 0) {
+    console.log("Your to-do list is empty!")
+  } else {
+    console.log(toDoList)
+  }
 }
 
 // add: adds a to-do item to the list; all words are entered as one item (parameter [3])
 
 function addToList(list, details) {
-  list.push(`${details}`);
-  fs.writeFile('list.txt', list, function (err) {
-    if (err) throw error;
-    console.log('Success!')
-  });  
-  console.log(list);
-  // console.log(`Your new to-do list is: ${list}`);
+  const stats = fs.statSync("list.txt");
+  const fileSizeInBytes = stats.size;
+  if (fileSizeInBytes === 0) {
+    fs.writeFile('list.txt', details, function (err) {
+      if (err) throw error;
+      console.log('Success!')
+    }); 
+  } else {
+    list.push(`${details}`);
+    fs.writeFile('list.txt', list, function (err) {
+      if (err) throw error;
+      console.log('Success!')
+    });  
+  }
   return list;
 }
 
@@ -42,6 +55,13 @@ function addToList(list, details) {
 
 // reset: removes all items from the to-do list
 
+function resetList() {
+  fs.writeFile('list.txt', '', function (err) {
+    if (err) throw error;
+    console.log('Success!')
+  });  
+}
+
 function main() {  
     
   if (choice === 'help') {
@@ -50,6 +70,8 @@ function main() {
     displayList();
   } else if (choice === 'add') {
     addToList(toDoList, details);
+  } else if (choice === 'reset') {
+    resetList();
   }
 }
 
